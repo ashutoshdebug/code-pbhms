c7f8c9a71770914a0e01e5df9e102e2d882bce30
ashutoshdebug <ashutoshkumart82@gmail.com>
2025-11-17 15:02:59 +0530

    More additions

--- pbhms/guests/forms.py
+++ pbhms/guests/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.utils import timezone
 from .models import CheckIn
 
 class checkin_form(forms.ModelForm):
-    checkin_form = forms.Model
+    class Meta:
+        model = CheckIn
+        fields = "__all__"
+        widgets = {
+            'room_type': forms.Select(attrs={'class': "bg-black text-white rounded"}),
+            'name': forms.TextInput(attrs={'placeholder': "Enter the name of the Guest", 'class': 'w-[100%] focus:outline-none rounded p-1'}),
+            'date': forms.DateTimeInput(attrs={'value': timezone.now().strftime('%Y-%m-%d %H:%M:%S'), 'readonly': 'readonly', 'class': 'focus:outline-none rounded p-1'}),
+            'aadhaar_card': forms.TextInput(attrs={'placeholder': "Enter the Aadhaar Card number of the Guest", 'class': 'w-[100%] focus:outline-none rounded p-1', 'minlength':12}),
+            'mobile': forms.TextInput(attrs={'placeholder': "Enter the mobile number of the Guest", 'class': 'w-[100%] focus:outline-none rounded p-1', 'minlength':10}),
+            'alloted_room': forms.TextInput(attrs={'placeholder': 'Enter the alloted room number of the Guest', 'class': 'w-[100%] focus:outline-none rounded p-1', 'minlength':3})
+            
+        }
+
+
+# class checkout_form(forms.Form):
+#     checkout_form = forms.CharField(label = "Your name", max_length=100, required=False)
--- pbhms/guests/views.py
+++ pbhms/guests/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CheckIn
+from .forms import checkin_form
 
 # Create your views here.
 def guests(request):
     return render(request, 'guests.html')
 
 def checkin(request):
-    if request.method == 'POST':
-        form = CheckInForm(request.POST)
+    # return render(request, 'guesthandling/checkin.html') # Direct loading of the page
+    guest = None
+    aadhaar_search = request.GET.get("aadhaar_search")
+    if aadhaar_search:
+        try:
+            guest = CheckIn.objects.get(aadhaar_card = aadhaar_search)
+        except CheckIn.DoesNotExist:
+            guest = None
+
+    if request.method == "POST":
+        form = checkin_form(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Guest checked in successfully!")
-            return redirect('guests:checkin')  # reload form after submission
-        else:
-            messages.error(request, "Please correct the errors below.")
-    else:
-        form = CheckInForm()
+            # room_type = form.cleaned_data['room_type']
+            # date = form.cleaned_data['date']
+            # name = form.cleaned_data['name']
+            # aadhaar_card = form.cleaned_data['aadhaar_card']
+            # mobile = form.cleaned_data['mobile']
+            # alloted_room = form.cleaned_data['alloted_room']
+            return redirect('guests:checkin')
 
-    return render(request, 'guesthandling/checkin.html', {'form': form})
+    else:
+        form = checkin_form()
+    return render(request, 'guesthandling/checkin.html', {'form': form, 'guest': guest,})
+        
 
 def checkout(request):
     return render(request, 'guesthandling/checkout.html')
